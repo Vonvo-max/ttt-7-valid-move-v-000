01da4f96530c75d6fe4fdbf303821ede5626799a
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-28 14:48:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,11 +13,11 @@ end
 
 
 #you dont always need to call on both arguments in a method for each line of code you written
-#you have to properly leverage the between method by asking it to call something, ca 
+#you have to properly leverage the between method by asking it to call something, ca
 # helper methods are needed in order to correctly point your code towards other methods previously defined
 #dry code - dont repeat yourself
-#single responsibility principle - writing individual methods 
-#you just need one end to end your entire block, don't need 
+#single responsibility principle - writing individual methods
+#you just need one end to end your entire block, don't need
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 board = [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "]
